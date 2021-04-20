61ec43fc56522c71b1e170b562ef64cd344ad2e9
aitx7270 <Aitx7270@co-well.com.vn>
2021-04-20 14:02:39 +0700

    add mockdata

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <p>This is an example of a simple HTML page with one paragraph.</p>
+    </body>
+</html>
